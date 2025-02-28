b2ee88e26ecb95e98f8dcfcf387b5d6ddd497dba
starifly <2003879+starifly@users.noreply.github.com>
2025-02-28 21:55:07 +0800

    add tls fragment

--- option/direct.go
+++ option/direct.go
@@ -12,6 +12,12 @@ type DirectInboundOptions struct {
 	Network         NetworkList `json:"network,omitempty"`
 	OverrideAddress string      `json:"override_address,omitempty"`
 	OverridePort    uint16      `json:"override_port,omitempty"`
+  Fragment        *Fragment `json:"fragment,omitempty"`
+}
+
+type Fragment struct {
+	Length   string `json:"length"`
+	Interval string `json:"interval"`
 }
 
 type _DirectOutboundOptions struct {
--- protocol/direct/outbound.go
+++ protocol/direct/outbound.go
@@ -2,8 +2,13 @@ package direct
 
 import (
 	"context"
+  "crypto/rand"
+  "encoding/binary"
+  "math/big"
 	"net"
 	"net/netip"
+  "strconv"
+  "strings"
 	"time"
 
 	"github.com/sagernet/sing-box/adapter"
@@ -39,6 +44,14 @@ type Outbound struct {
 	overrideOption      int
 	overrideDestination M.Socksaddr
 	// loopBack *loopBackDetector
+  fragment            *Fragment
+}
+
+type Fragment struct {
+	MinInterval int32
+	MaxInterval int32
+	MinLength   int32
+	MaxLength   int32
 }
 
 func NewOutbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.DirectOutboundOptions) (adapter.Outbound, error) {
@@ -70,6 +83,58 @@ func NewOutbound(ctx context.Context, router adapter.Router, logger log.ContextL
 		outbound.overrideOption = 3
 		outbound.overrideDestination = M.Socksaddr{Port: options.OverridePort}
 	}
+  if options.Fragment != nil {
+		if len(options.Fragment.Interval) == 0 || len(options.Fragment.Length) == 0 {
+			return nil, E.New("Invalid interval or length")
+		}
+		intervalMinMax := strings.Split(options.Fragment.Interval, "-")
+		var minInterval, maxInterval int64
+		var err, err2 error
+		if len(intervalMinMax) == 2 {
+			minInterval, err = strconv.ParseInt(intervalMinMax[0], 10, 64)
+			maxInterval, err2 = strconv.ParseInt(intervalMinMax[1], 10, 64)
+		} else {
+			minInterval, err = strconv.ParseInt(intervalMinMax[0], 10, 64)
+			maxInterval = minInterval
+		}
+		if err != nil {
+			return nil, E.Cause(err, "Invalid minimum interval: ")
+		}
+		if err2 != nil {
+			return nil, E.Cause(err2, "Invalid maximum interval: ")
+		}
+
+		lengthMinMax := strings.Split(options.Fragment.Length, "-")
+		var minLength, maxLength int64
+		if len(lengthMinMax) == 2 {
+			minLength, err = strconv.ParseInt(lengthMinMax[0], 10, 64)
+			maxLength, err2 = strconv.ParseInt(lengthMinMax[1], 10, 64)
+
+		} else {
+			minLength, err = strconv.ParseInt(lengthMinMax[0], 10, 64)
+			maxLength = minLength
+		}
+		if err != nil {
+			return nil, E.Cause(err, "Invalid minimum length: ")
+		}
+		if err2 != nil {
+			return nil, E.Cause(err2, "Invalid maximum length: ")
+		}
+
+		if minInterval > maxInterval {
+			minInterval, maxInterval = maxInterval, minInterval
+		}
+		if minLength > maxLength {
+			minLength, maxLength = maxLength, minLength
+		}
+
+		outbound.fragment = &Fragment{
+			MinInterval: int32(minInterval),
+			MaxInterval: int32(maxInterval),
+			MinLength:   int32(minLength),
+			MaxLength:   int32(maxLength),
+		}
+	}
 	return outbound, nil
 }
 
@@ -99,6 +164,16 @@ func (h *Outbound) DialContext(ctx context.Context, network string, destination 
 		return nil, err
 	}
 	return h.loopBack.NewConn(conn), nil*/
+  if network == N.NetworkTCP && h.fragment != nil {
+		conn = &FragmentedClientHelloConn{
+			Conn:        conn,
+			ctx:         ctx,
+			logger:      h.logger,
+			maxLength:   int(h.fragment.MaxLength),
+			minInterval: time.Duration(h.fragment.MinInterval) * time.Millisecond,
+			maxInterval: time.Duration(h.fragment.MaxInterval) * time.Millisecond,
+		}
+	}
 	return h.dialer.DialContext(ctx, network, destination)
 }
 
@@ -170,7 +245,18 @@ func (h *Outbound) DialParallel(ctx context.Context, network string, destination
 			return nil, E.New("no IPv6 address available for ", destination)
 		}
 	}
-	return dialer.DialParallelNetwork(ctx, h.dialer, network, destination, destinationAddresses, domainStrategy == dns.DomainStrategyPreferIPv6, nil, nil, nil, h.fallbackDelay)
+  conn, err := dialer.DialParallelNetwork(ctx, h.dialer, network, destination, destinationAddresses, domainStrategy == dns.DomainStrategyPreferIPv6, nil, nil, nil, h.fallbackDelay)
+  if network == N.NetworkTCP && h.fragment != nil {
+		conn = &FragmentedClientHelloConn{
+			Conn:        conn,
+			ctx:         ctx,
+			logger:      h.logger,
+			maxLength:   int(h.fragment.MaxLength),
+			minInterval: time.Duration(h.fragment.MinInterval) * time.Millisecond,
+			maxInterval: time.Duration(h.fragment.MaxInterval) * time.Millisecond,
+		}
+	}
+	return conn, nil 
 }
 
 func (h *Outbound) DialParallelNetwork(ctx context.Context, network string, destination M.Socksaddr, destinationAddresses []netip.Addr, networkStrategy *C.NetworkStrategy, networkType []C.InterfaceType, fallbackNetworkType []C.InterfaceType, fallbackDelay time.Duration) (net.Conn, error) {
@@ -253,3 +339,97 @@ func (h *Outbound) NewPacketConnection(ctx context.Context, conn N.PacketConn, m
 	return NewPacketConnection(ctx, h, conn, metadata)
 }
 */
+
+// stolen from github.com/xtls/xray-core/transport/internet/reality
+func randBetween(left int64, right int64) int64 {
+	if left == right {
+		return left
+	}
+	bigInt, _ := rand.Int(rand.Reader, big.NewInt(right-left))
+	return left + bigInt.Int64()
+}
+
+type FragmentedClientHelloConn struct {
+	net.Conn
+	ctx         context.Context
+	logger      log.ContextLogger
+	PacketCount int
+	minLength   int
+	maxLength   int
+	minInterval time.Duration
+	maxInterval time.Duration
+}
+
+func (c *FragmentedClientHelloConn) Write(b []byte) (n int, err error) {
+	if c.PacketCount == 0 {
+		if len(b) >= 5 && b[0] == 22 {
+			n, err = sendFragmentedClientHello(c, b, c.minLength, c.maxLength)
+		} else {
+			n, err = c.Conn.Write(b)
+		}
+
+		if err == nil {
+			c.PacketCount++
+		}
+
+		return
+	}
+
+	return c.Conn.Write(b)
+}
+
+func (c *FragmentedClientHelloConn) Upstream() any {
+	return c.Conn
+}
+
+func sendFragmentedClientHello(conn *FragmentedClientHelloConn, clientHello []byte, minFragmentSize, maxFragmentSize int) (n int, err error) {
+	if len(clientHello) < 5 || clientHello[0] != 22 {
+		return 0, E.New("not a valid TLS ClientHello message")
+	}
+
+	clientHelloLen := (int(clientHello[3]) << 8) | int(clientHello[4])
+	if conn.logger != nil {
+		conn.logger.InfoContext(conn.ctx, "Sending fragmented TLS client hello: ", clientHelloLen)
+	}
+
+	clientHelloData := clientHello[5:]
+	i := 0
+	for {
+		fragmentEnd := i + int(randBetween(int64(minFragmentSize), int64(maxFragmentSize)))
+		if fragmentEnd > clientHelloLen {
+			fragmentEnd = clientHelloLen
+		}
+
+		fragment := clientHelloData[i:fragmentEnd]
+		i = fragmentEnd
+
+		err = writeFragmentedRecord(conn, 22, fragment, clientHello)
+		if err != nil {
+			return 0, err
+		}
+
+		if i >= clientHelloLen {
+			break
+		}
+
+		randomInterval := randBetween(int64(conn.minInterval), int64(conn.maxInterval))
+		if randomInterval > 0 {
+			time.Sleep(time.Duration(randomInterval))
+		}
+	}
+
+	return len(clientHello), nil
+}
+
+func writeFragmentedRecord(c *FragmentedClientHelloConn, contentType uint8, data []byte, clientHello []byte) error {
+	header := make([]byte, 5)
+	header[0] = byte(clientHello[0])
+
+	tlsVersion := (int(clientHello[1]) << 8) | int(clientHello[2])
+	binary.BigEndian.PutUint16(header[1:], uint16(tlsVersion))
+
+	binary.BigEndian.PutUint16(header[3:], uint16(len(data)))
+	_, err := c.Conn.Write(append(header, data...))
+
+	return err
+}
