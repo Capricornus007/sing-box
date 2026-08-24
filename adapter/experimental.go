17cc25da9ac1f64ffa3930cd557530defb99db4c
那個呆萌在山外 <5h1n38r16h7.d14m0nd@gmail.com>
2026-08-24 17:24:41 +0900

    Fix URLTest group hang on unresponsive outbound
    Cherry-pick of upstream e4a4e8c79 (SagerNet/sing-box). Kept fork deps
    (sing v0.9.0-beta.4, ahead of upstream's v0.8.14 bump). Dropped the
    duplicate URLTest.PerformUpdateCheck already present in the fork.
    (cherry picked from commit e4a4e8c79)

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -167,6 +167,7 @@ type OutboundGroup interface {
 type URLTestGroup interface {
 	OutboundGroup
 	URLTest(ctx context.Context) (map[string]uint16, error)
+	PerformUpdateCheck()
 }
 
 func OutboundTag(detour Outbound) string {
